5f543ae4f3df02d39f21ca2821a1eb4b155b5ee8
Janik von Rotz <login@janikvonrotz.ch>
2021-01-13 18:45:00 +0100

    init

---
+++ README.rst
@@ -0,0 +1,18 @@
+===========================
+Website Product Description
+===========================
+
+.. image:: ./static/description/icon.png
+  :width: 100
+  :alt: Icon
+
+Add separate description field to website product view.
+
+Usage
+~~~~~
+
+Clone module into Odoo addon directory.
+
+.. code-block:: bash
+
+    git clone git@github.com:mint-system/odoo-app-website-product-description.git ./addons/website_product_description
---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Invoice Description",
+
+    'summary': """
+        Add separate description field to website product view.
+    """,
+
+    'description': """
+        Add separate description field to website product view.
+    """,
+
+    'author': "Mint System GmbH",
+    'website': "https://www.mint-system.ch",
+    'category': 'Accounting',
+    'version': '14.0.1.0.0',
+
+    'depends': ['website_sale'],
+
+    'data': [
+        'views/product_product_normal_form_view.xml',
+        'views/product_product_template_only_form_view.xml',
+        'views/website_sale_product.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
